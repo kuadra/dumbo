6ce69c1248b93e63fa58a94235a512f7d9fa38b1
kuadra <kuadra@protonmail.com>
2022-03-26 17:22:38 +0100

    fuck java

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ async fn main() {
     loop {
         // The second item contains the IP and port of the new connection.
         match listener.accept().await{
-            Ok((mut socket, _addr)) =>  socket.write_all(writeAddress(_addr).as_bytes()).await.unwrap(),
+            Ok((mut socket, _addr)) =>  socket.write_all(write_address(_addr).as_bytes()).await.unwrap(),
             Err(e) => println!("couldn't get client: {:?}", e),
         }
     }
 }
 
-fn writeAddress(_addr : SocketAddr) -> String {
+fn write_address(_addr : SocketAddr) -> String {
     String::from(format!("{}\n",_addr))
 }
