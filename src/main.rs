6b79214f4e93207fdca7fd8521a2c2b356e971f5
kuadra <kuadra@protonmail.com>
2022-03-26 17:22:07 +0100

    write address

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 use tokio::net::{TcpListener};
 use tokio::io::AsyncWriteExt;
 
@@ -5,12 +7,16 @@ use tokio::io::AsyncWriteExt;
 async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
+    
     loop {
         // The second item contains the IP and port of the new connection.
         match listener.accept().await{
-            Ok((mut socket, _addr)) =>  socket.write_all(b"ahah2").await.unwrap(),
+            Ok((mut socket, _addr)) =>  socket.write_all(writeAddress(_addr).as_bytes()).await.unwrap(),
             Err(e) => println!("couldn't get client: {:?}", e),
         }
     }
+}
+
+fn writeAddress(_addr : SocketAddr) -> String {
+    String::from(format!("{}\n",_addr))
 }
