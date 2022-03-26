81431735ec25200a1212c8d44e85363fb7cc2f37
kuadra <kuadra@protonmail.com>
2022-03-26 17:28:34 +0100

    fuck java

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use tokio::io::AsyncWriteExt;
 #[tokio::main]
 async fn main() {
     // Bind the listener to the address
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let listener = TcpListener::bind("123123:6379").await.unwrap();
     
     loop {
         // The second item contains the IP and port of the new connection.
         match listener.accept().await{
-            Ok((mut socket, _addr)) =>  socket.write_all(write_address(_addr).as_bytes()).await.unwrap(),
+            Ok((mut socket, _addr)) =>  socket.write_all(&write_address2(_addr)).await.unwrap(),
             Err(e) => println!("couldn't get client: {:?}", e),
         }
     }
@@ -19,4 +19,9 @@ async fn main() {
 
 fn write_address(_addr : SocketAddr) -> String {
     String::from(format!("{}\n",_addr))
+}
+
+
+fn write_address2(addr : SocketAddr) -> Vec<u8> {
+    format!("{}", addr).into_bytes()
 }
