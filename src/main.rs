6af552725ccefe685ec525d1d096721a07e90dd5
kuadra <kuadra@protonmail.com>
2022-03-27 01:35:29 +0100

    attese

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
 use std::net::SocketAddr;
 
-use tokio::net::{TcpListener};
-use tokio::io::AsyncWriteExt;
+use tokio::{net::{TcpListener, TcpStream}, io::{AsyncWriteExt, AsyncReadExt}};
 
 #[tokio::main]
 async fn main() {
     // Bind the listener to the address
-    let listener = TcpListener::bind("0.0.0.0:6379").await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     
     loop {
+        println!("meh1");
         // The second item contains the IP and port of the new connection.
         match listener.accept().await{
-            Ok((mut socket, _addr)) =>  socket.write_all(&write_address2(_addr)).await.unwrap(),
+            Ok((stream, addr)) =>  handle(stream, addr).await,
             Err(e) => println!("couldn't get client: {:?}", e),
         }
     }
 }
 
-fn write_address2(addr : SocketAddr) -> Vec<u8> {
-    format!("{}\n", addr).into_bytes()
+
+async fn handle(mut stream : TcpStream, addr:  SocketAddr) {
+    stream.write_all(&addr.stocazzoo()).await.unwrap();
+    println!("meh2 {}", stream.read_to_string(&mut String::new()).await.unwrap());
+}
+
+trait ASD{
+    fn stocazzoo(&self) -> Vec<u8>;
+}
+
+
+impl ASD for SocketAddr {
+    fn stocazzoo(&self) -> Vec<u8> {
+        println!("ppp");
+        format!("{:?}\n", self).into_bytes()
+    }
 }
