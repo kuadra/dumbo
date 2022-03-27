19d0c8ecc0025988fa811cfd17ba9b58c8ea9b32
kuadra <kuadra@protonmail.com>
2022-03-27 19:43:57 +0200

    eh

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,47 @@ use tokio::{
 };
 
 #[tokio::main]
- async fn main() {
+async fn main() {
     match TcpListener::bind("127.0.0.1:6379").await {
         Ok(res) => handle_connection(res).await,
-        Err(err) => println!("Binding error")
+        Err(err) => println!("Binding error"),
     }
 }
 
-async fn handle_connection(listener : TcpListener) {
-    println!("{:?}",listener);
-    loop{
+async fn handle_connection(listener: TcpListener) {
+    println!("{:?}", listener);
+    loop {
         match listener.accept().await {
-            Ok(res) => println!("ohohoho")  ,
-            Err(err) => println!("Binding error")  
+            Ok((stream, addr)) => handle_stream(stream).await.unwrap(),
+            Err(err) => println!("Binding error"),
         }
     }
+}
+
+async fn handle_stream(stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    loop {
+        // Wait for the socket to be readable
+        stream.readable().await?;
+
+        // Creating the buffer **after** the `await` prevents it from
+        // being stored in the async task.
+        let mut buf = [0; 4096];
+
+        // Try to read data, this may still fail with `WouldBlock`
+        // if the readiness event is a false positive.
+        match stream.try_read(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => {
+                println!("read {} bytes", n);
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
+    }
+    Ok(())
 }
