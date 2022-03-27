fde46dea252dd31973c9ec194bbe9f635a1e6861
kuadra <kuadra@protonmail.com>
2022-03-27 19:20:55 +0200

    mo ho capito

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-use tokio::net::TcpListener;
+use std::error::Error;
+use tokio::{
+    io,
+    net::{TcpListener, TcpStream},
+};
 
 #[tokio::main]
-async fn main(){
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+ async fn main() {
+    match TcpListener::bind("127.0.0.1:6379").await {
+        Ok(res) => handle_connection(res).await,
+        Err(err) => println!("Binding error")
+    }
+}
 
-    loop {
-        let a = listener.accept();
-        let b = a.await;
-        match b {
-            Ok(res) => {
-                println!("{:?}", res);
-                println!("new client: {:?}", res.1)
-            }
-            Err(err) => println!("{}", err),
+async fn handle_connection(listener : TcpListener) {
+    println!("{:?}",listener);
+    loop{
+        match listener.accept().await {
+            Ok(res) => println!("ohohoho")  ,
+            Err(err) => println!("Binding error")  
         }
     }
 }
