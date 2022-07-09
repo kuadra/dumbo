6872a92698bbc17a472be7a2245d8409ed9a204b
kuadra <kuadra@protonmail.com>
2022-07-09 15:48:08 +0200

    read/write

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,36 @@ use tokio::{net::{TcpListener, TcpStream}, io::AsyncReadExt};
 
 
 #[tokio::main]
-async fn main(){
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+async fn main() {
+    let bound = TcpListener::bind("127.0.0.1:6379");
+    let listener = bound.await.unwrap();
 
     loop {
-        println!("===LOOP===");        
+        println!("===LOOP===");
         let a = listener.accept();
         let b = a.await;
         match b {
             Ok(res) => {
-                //println!("{:?}", res.0);
                 println!("New client: {:?}", res.1);
                 handle(res.0).await;
-                res.0.writable().await;
-                res.0.try_write(b"asd");
             }
             Err(err) => println!("{}", err),
         }
     }
 }
 
-async fn handle(mut stream : TcpStream) {
-    let a = &mut String::new();
-    stream.read_to_string(a).await.unwrap();
-    println!("{}",a);
+async fn handle(stream: TcpStream) {
+    
+    stream.writable().await.unwrap();
+    match stream.try_write(b"asd\n") {
+        Ok(n) => println!("Wrote {} bytes", n),
+        Err(e) => println!("{:?}", e),
+    }
+    stream.readable().await.unwrap();
+    let mut a = [0;10];
+    match stream.try_read(&mut a) {
+        Ok(n) => println!("Read {} bytes", n),
+        Err(e) => println!("{:?}", e),
+    }
+    println!("Buffer A: {:?}", a);
 }
