05386a52d390a796b9e7f09d7f46ada9b0e879e5
kuadra <kuadra@protonmail.com>
2022-07-09 18:18:20 +0200

    buffo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod buffo;
+
 use tokio::{net::{TcpListener, TcpStream}, io::AsyncReadExt};
 
 
@@ -21,17 +23,11 @@ async fn main() {
 }
 
 async fn handle(stream: TcpStream) {
-    
-    //stream.writable().await.unwrap();
-    match stream.try_write(b"asd\n") {
-        Ok(n) => println!("Wrote {} bytes", n),
-        Err(e) => println!("{:?}", e),
-    }
-    //stream.readable().await.unwrap();
-    let mut a = [0;10];
-    match stream.try_read(&mut a) {
+    stream.readable().await.unwrap();
+    let mut buf : buffo::Buffo = buffo::Buffo::new();
+    match stream.try_read(buf.get_mem()) {
         Ok(n) => println!("Read {} bytes", n),
         Err(e) => println!("Error: {:?}", e),
     }
-    println!("Buffer A: {:?}", String::from_utf8(a.to_vec()));
+    println!("Buffer A: {:?}", String::from_utf8(buf.get_mem().to_vec()));
 }
