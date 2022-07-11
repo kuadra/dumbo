527733eeff5d170272c9016586f771c35f10438c
kuadra <kuadra@protonmail.com>
2022-07-11 11:23:08 +0200

    ppp

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,35 @@
 mod buffo;
 use std::io::Result;
 
-use tokio::{net::{TcpListener, TcpStream}, io::{AsyncReadExt, AsyncWriteExt}};
-
+use buffo::Buffo;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
 #[tokio::main]
 async fn main() {
     let bound = TcpListener::bind("127.0.0.1:6379");
     let listener = bound.await.unwrap();
-    let buffer = Buffo::new();
+    let mut buffer = Buffo::new();
 
-    //buffer_scrittura
-    wait_for_data(listener, buffer).await.unwrap();
-    
-    //scrivi data(buffer scrittura)
-}
-
-async fn wait_for_data(listener: TcpListener, buffer: Buffo) -> Result<String> {
-    loop {
+    loop{
         println!("===LOOP===");
-        let connection = listener.accept().await;
-        match connection {
-            Ok((stream, addr)) => {
-                println!("New client: {:?}", addr);
-                tokio::spawn(async move {
-                    handle(stream).await;
-                });
-            }
-            Err(err) => return Err(err),
-        }
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle(stream, buffer).await;
+        });
     }
 }
 
-async fn handle(mut stream: TcpStream) {
+async fn handle(mut stream: TcpStream, mut buffer: Buffo) -> Buffo{
     stream.readable().await.unwrap();
     match stream.try_read(buffer.get_mem()) {
         Ok(n) => {
             println!("Read {} bytes", n);
-            println!("Buffer A: {:?}", String::from_utf8(buffer.get_mem().to_vec()));
-            return 1;
         }
         Err(e) => {
             println!("Error: {:?}", e);
-            return 0;
         }
     }
 
@@ -51,5 +38,6 @@ async fn handle(mut stream: TcpStream) {
     stream.flush().await.unwrap();
     println!("STREAM FLUSHED");
     tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
-    println!("Buffer A: {:?}", String::from_utf8(buf.get_mem().to_vec()));
+    println!("Buffer A: {:?}", String::from_utf8(buffer.get_mem().to_vec()));
+    buffer
 }
